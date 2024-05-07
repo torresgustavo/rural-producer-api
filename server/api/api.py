0065a9261e34bab8a15cedffbd74cf9623ca55f1
Gustavo Alex Torres <gustavoalextorres@gmail.com>
2024-05-07 02:55:54 -0300

    chore: Inicializando rotas de produtores rurais

--- server/api/api.py
+++ server/api/api.py
@@ -1,8 +1,9 @@
 from ninja import NinjaAPI
 from django.http.request import HttpRequest
-from api.errors.base_error import BaseError 
+from api.errors.base_error import BaseError
 
-api = NinjaAPI(title='Rural Producers API')
+api = NinjaAPI(title="Rural Producers API")
+
 
 @api.exception_handler(BaseError)
 def handling_base_error(request: HttpRequest, exception: BaseError):
@@ -12,4 +13,6 @@ def handling_base_error(request: HttpRequest, exception: BaseError):
         status=exception.status.value,
     )
 
-api.add_router('/document-types', "api.views.document_view.router")
+
+api.add_router("/document-types", "api.views.document_view.router")
+api.add_router("/rural-producers", "api.views.rural_producer_view.router")
