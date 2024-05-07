2ed8eef80fae5f9a8f65c1f0650be510b914afaf
Gustavo Alex Torres <gustavoalextorres@gmail.com>
2024-05-07 02:22:01 -0300

    refactor: Removendo django rest framework e substituindo por django ninja

--- server/config/settings.py
+++ server/config/settings.py
@@ -42,15 +42,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "rest_framework",
     "api"
 ]
-
-# REST_FRAMEWORK = {
-#     'DEFAULT_PERMISSION_CLASSES': [
-#         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-#     ]
-# }
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
